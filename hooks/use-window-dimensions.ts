bbfa57acc27b70398e68591beb4dbe9b99ed7e2b
sjtommylee <sjtommylee@gmail.com>
2023-03-01 02:37:15 -0800

    wip

--- hooks/use-window-dimensions.ts
+++ hooks/use-window-dimensions.ts
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 export function getWindowDimensions() {
-  return {};
+  return {
+    test: "baz",
+  };
 }
 
 export const useWindowDimensions = () => {
   const [windowDimensions] = useState(getWindowDimensions());
+  console.log(windowDimensions);
   return windowDimensions;
 };
