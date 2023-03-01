a981637f39f8924d73abcf8eb9337c563ad9ded1
sjtommylee <sjtommylee@gmail.com>
2023-02-28 23:48:33 -0800

    SEO file with metadata api

---
+++ app/SEO.tsx
@@ -0,0 +1,10 @@
+// using the new metadata API
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "",
+    template: "",
+  },
+  description: "",
+};
