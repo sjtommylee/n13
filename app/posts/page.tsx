e160a33057e401de54246532d3f3a95a98f73a2e
sjtommylee <sjtommylee@gmail.com>
2023-03-01 00:38:36 -0800

    sub directories

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const BlogPage: NextPage = () => {
+  return (
+    <section>
+      <div>
+        <h1>Blog Page / Posts</h1>
+      </div>
+    </section>
+  );
+};
+
+export default BlogPage;
