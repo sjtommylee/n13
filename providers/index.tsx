be1971a20905c15b8c242a665d34a345b87e91cb
sjtommylee <sjtommylee@gmail.com>
2023-03-01 17:26:04 -0800

    swr, theme provider

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,7 +1,24 @@
+import { ThemeProvider } from "next-themes";
+import { SWRConfig } from "swr";
 interface Props {
   children: React.ReactNode;
 }
 
+const fetchJSON = () => {
+  return {};
+};
+
 export const RootProvider = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetchJSON,
+        onError: (err) => {
+          console.log(err);
+        },
+      }}
+    >
+      <ThemeProvider>{children}</ThemeProvider>;
+    </SWRConfig>
+  );
 };
