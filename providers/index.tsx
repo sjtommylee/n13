52ecbbb49d4503a7b93986161f5433a97d670675
sjtommylee <sjtommylee@gmail.com>
2023-03-01 17:37:10 -0800

    use is mounted hook, providers

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,3 +1,4 @@
+import { useIsMounted } from "@/hooks/use-is-mounted";
 import { ThemeProvider } from "next-themes";
 import { SWRConfig } from "swr";
 interface Props {
@@ -5,10 +6,12 @@ interface Props {
 }
 
 const fetchJSON = () => {
+  // TODO - refactor this into separate .ts file
   return {};
 };
 
 export const RootProvider = ({ children }: Props) => {
+  const isMounted = useIsMounted();
   return (
     <SWRConfig
       value={{
@@ -18,7 +21,7 @@ export const RootProvider = ({ children }: Props) => {
         },
       }}
     >
-      <ThemeProvider>{children}</ThemeProvider>;
+      {isMounted() && <ThemeProvider>{children}</ThemeProvider>}
     </SWRConfig>
   );
 };
