cd32c023407400d0510c502f6edfbde22f831d4d
sjtommylee <sjtommylee@gmail.com>
2023-03-03 15:31:29 -0800

    toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -2,5 +2,10 @@ import * as React from "react";
 import { useTheme } from "next-themes";
 
 export const ToggleTheme = () => {
+  const { setTheme } = useTheme();
+
+  const handleLight = () => {};
+  const handleDark = () => {};
+
   return <div></div>;
 };
