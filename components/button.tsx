7bb7f22fb5120593f363c8db1ce5fdb48405b58c
sjtommylee <sjtommylee@gmail.com>
2023-03-02 16:16:51 -0800

    button primitive

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { cva, VariantProps } from "class-variance-authority";
 
-export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface Props
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof themeVariant> {}
 
-const variants = {};
+const themeVariant = cva("", {
+  variants: {},
+});
 
 const Button = React.forwardRef<HTMLButtonElement, Props>(({ className, ...props }, ref) => {
-  return <button>button</button>;
+  return <button className="">button</button>;
 });
 
 Button.displayName = "Button";
 
-export { Button, variants };
+export { Button, themeVariant };
