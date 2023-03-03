d18fd4c589872d81f7ff97265e7b53e0fd16950a
sjtommylee <sjtommylee@gmail.com>
2023-03-02 19:18:06 -0800

    utility type guards

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
 export const isArray = Array.isArray;
-export const isNumber = (val: unknown): val is number => typeof val === "number";
-export const isBoolean = () => {};
+export const isObject = (value: unknown): value is Record<any, any> =>
+  value !== null && typeof value === "object";
+export const isNumber = (value: unknown): value is number => typeof value === "number";
+export const isBoolean = (value: unknown): value is Boolean => typeof value === "boolean";
+export const isElement = (value: unknown): value is Element => isObject(value);
+export const isString = (value: unknown): value is string => typeof value === "string";
